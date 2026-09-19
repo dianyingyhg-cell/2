917f110236a0a8bc1d7010dd88c7b5a90458fbab
dianyingyhg-cell <dianyingyhg@gmail.com>
2026-09-19 21:13:54 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,17 @@ android {
         versionName = "0.2.0"
     }
 
-    buildFeatures {
-        viewBinding = false
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
         jvmTarget = "17"
+    }
+
+    buildFeatures {
+        viewBinding = false
     }
 }
 
